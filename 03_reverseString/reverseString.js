69596031bda07ed5f67aaa1da2e626a8e4748fa2
cohedx7 <kimkh87@gmail.com>
2022-03-17 17:47:24 +0100

    js exercise 3 done (difficult)...

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
 
+const reverseString = function(word) {
+    return word.split("").reverse().join("");
 };
+
+/*
+const reverseString = function(word) {
+    let result = "";
+    for (let i = (word.length - 1); i >= 0; i--) {
+        let letter = word.charAt[i];
+        result = result.concat(letter);
+    }
+    return result;
+};
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
