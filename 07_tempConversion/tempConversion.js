f18e44d8d25a37a704907ec7ef31f251eb627e06
cohedx7 <kimkh87@gmail.com>
2022-03-21 16:30:56 +0100

    exercise 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    let convertedTemp;
+    convertedTemp = (temp - 32) * (5/9);
+    roundedCTemp = Math.round(convertedTemp * 10) / 10;
+    return roundedCTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    let convertedTemp;
+    convertedTemp = temp * (9/5) + 32;
+    roundedCTemp = Math.round(convertedTemp * 10) / 10;
+    return roundedCTemp;
 };
 
 // Do not edit below this line
