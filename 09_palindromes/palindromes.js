a6cda8212f950b908266fae06007bb6719d3602a
cohedx7 <kimkh87@gmail.com>
2022-03-28 22:42:30 +0200

    done exercise 09, confusing...

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    input = word.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+        input
+            .split('')
+            .reverse()
+            .join('') == input
+    );
 };
 
 // Do not edit below this line
