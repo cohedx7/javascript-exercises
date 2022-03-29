3ad3325691b6826b5ea18e715bb5d99c06459bce
cohedx7 <kimkh87@gmail.com>
2022-03-29 17:30:00 +0200

    exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let firstN = 0;
+    let secondN = 1;
+    let nextN;
+    if (input < 0) return "OOPS"
+    else {
+        let i = 1;
+        do {
+            nextN = firstN + secondN;
+            firstN = secondN;
+            secondN = nextN;
+            i++;
+        } while (i < Number(input));
+        return nextN;
+    }
 };
 
 // Do not edit below this line
