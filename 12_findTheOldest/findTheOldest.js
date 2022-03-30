f952494e58e10291491c5696756bd669ed31c0e4
cohedx7 <kimkh87@gmail.com>
2022-03-30 17:42:45 +0200

    exercise 12 done...

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce(function(oldest, select) {
+       const oldAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+       const nowAge = getAge(select.yearOfBirth, select.yearOfDeath);
+       if (oldAge < nowAge) return select
+       else return oldest
+    })
 };
+
+function getAge(birthYear, deathYear) {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    } return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
